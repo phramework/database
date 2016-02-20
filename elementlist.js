d0161eac39d794c2921aedffb02eaf2b7484f576
Xenofon Spafaridis <nohponex@gmail.com>
2016-02-20 18:22:42 +0200

    Update doc

--- elementlist.js
+++ elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Phramework\\Database\\Database"],["c","Phramework\\Database\\IAdapter"],["c","Phramework\\Database\\MySQL"],["c","Phramework\\Database\\Operations\\Create"],["c","Phramework\\Database\\Operations\\Delete"],["c","Phramework\\Database\\Operations\\Update"],["c","Phramework\\Database\\PostgreSQL"]];
+ApiGen.elements = [["c","Phramework\\Database\\Database"],["c","Phramework\\Database\\IAdapter"],["c","Phramework\\Database\\MySQL"],["c","Phramework\\Database\\Operations\\Create"],["c","Phramework\\Database\\Operations\\Delete"],["c","Phramework\\Database\\Operations\\Update"],["c","Phramework\\Database\\PostgreSQL"],["c","Phramework\\Database\\SQLite"]];
